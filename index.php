c74bb4c6227c70e737f3834d5e87127b00e08e0d
Perevozchykov Kostiantyn <skvoz.ne@gmail.com>
2016-02-17 20:16:03 +0200

    commit onone

--- index.php
+++ index.php
@@ -1,41 +1,18 @@
 <?php
 //output  10kk string
 
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '2222';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
-echo '1111';
-echo '<br/>';
+echo '1111111111111111';
+echo '1111111111111111';
+echo '1111111111111111';
+echo '1111111111111111';
+echo '1111111111111111';
+echo '1111111111111111';
+echo '1111111111111111';
+echo '1111111111111111';
+echo '1111111111111111';
+echo '1111111111111111';
+echo '1111111111111111';
+echo '1111111111111111';
+echo '1111111111111111';
+echo '1111111111111111';
+
